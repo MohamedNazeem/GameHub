313d503b45562f3bd7347392fc59f41f41874e87
MohamedNazeem <m.amrnazeem@gmail.com>
2023-07-16 16:16:09 +0200

    implement infinite scrolling

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,5 @@
-import {
-  Alert,
-  AlertIcon,
-  Box,
-  Button,
-  SimpleGrid,
-  Spinner,
-} from "@chakra-ui/react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Alert, AlertIcon, Box, SimpleGrid, Spinner } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import { GameQuery } from "../App";
@@ -19,48 +13,53 @@ const GameGrid = ({ gameQuery }: Props) => {
     data: games,
     error,
     isLoading,
-    isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
   } = useGames(gameQuery);
 
+  const fetchedGamesCount =
+    games?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
-    <Box padding="10px">
-      {isLoading && (
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      )}
-
-      {error && (
-        <Alert status="error" justifyContent="center" textAlign="center">
-          <AlertIcon />
-          {error.message}
-        </Alert>
-      )}
-
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={10}
-        margin={3}
+    <Box>
+      <InfiniteScroll
+        dataLength={fetchedGamesCount}
+        hasMore={!!hasNextPage}
+        next={() => fetchNextPage()}
+        loader={<Spinner />}
       >
-        {games?.pages.map((page) => (
-          <>
-            {page.results.map((game) => (
-              <GameCard key={game.id} game={game} />
-            ))}
-          </>
-        ))}
-      </SimpleGrid>
-      {hasNextPage && (
-        <Button onClick={() => fetchNextPage()} marginY={5}>
-          {isFetchingNextPage ? "Loading..." : "Load More"}
-        </Button>
-      )}
+        {isLoading && (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        )}
+
+        {error && (
+          <Alert status="error" justifyContent="center" textAlign="center">
+            <AlertIcon />
+            {error.message}
+          </Alert>
+        )}
+
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+          spacing={10}
+          padding="10px"
+          margin={3}
+        >
+          {games?.pages.map((page) => (
+            <>
+              {page.results.map((game) => (
+                <GameCard key={game.id} game={game} />
+              ))}
+            </>
+          ))}
+        </SimpleGrid>
+      </InfiniteScroll>
     </Box>
   );
 };
